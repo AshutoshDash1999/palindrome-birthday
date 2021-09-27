28a60736058bfa3a417f4afad3c57042272a33af
Ashutosh Dash <dashashutosh1999@gmail.com>
2021-09-28 00:20:10 +0530

    tried creating own logic

--- script.js
+++ script.js
@@ -14,12 +14,18 @@ function validate(){
     var dobValue = dob.value;
     var dobList = ((dobValue.replaceAll('-', '')).split(''));
     dobList.splice(0, 2);
+    console.log(dobList)
+    var date = (dobList.splice(4,6).join(''));
+    var month = (dobList.splice(2,4).join(''));
+    var year = (dobList.splice(0,2).join(''));
+    console.log(date, month, year); 
     var dobStr = dobList.join('');
     var dobReversedList = (dobList.reverse()).join('');
     console.log(dobReversedList);
     console.log(dobStr);
     checkPalindrome(dobReversedList, dobStr);
 
+    
     
 }
 
